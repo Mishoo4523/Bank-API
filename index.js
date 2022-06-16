f9d3aa85a73d94f94d36be75ace28a13ace49de1
Mishoo4523 <mishomurad@gmail.com>
2022-06-16 22:28:04 +0300

    JSUT DO IT

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import expres from 'express';
-import bodyParser from 'body-parse';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js';
 
 const app = express();
-const port = 5000;
+const PORT = 5000;
 
-app.use(bodyParser.jason());
+app.use(bodyParser.json());
+
+app.use('/users', usersRoutes)
+
+
+app.get('/', (req,res) => res.send('Hello from Homepage.'));
+
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
